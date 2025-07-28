29e52e19b3c5b8806bfbfcb5af0523a24f340ca5
Henry <henrywilkerson24@gmail.com>
2025-07-28 09:38:48 -0700

    Your commit message here

---
+++ src/sensor.c
@@ -0,0 +1,19 @@
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/sys/printk.h>
+
+#define I2C0_NODE DT_NODELABEL(hdc3022)
+#define I2C_SPEED_STANDARD   (0x1U)
+
+static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C0_NODE);
+uint8_t data;
+
+if (!device_is_ready(dev_i2c.bus)) {
+    printk("I2C bus %s is not ready!\n\r",dev_i2c.bus->name);
+    return;
+}
+
+ret = i2c_read_dt(&dev_i2c, &data, sizeof(data));
+if(ret != 0){
+    printk("Failed to read from I2C device address %x at Reg. %x \n\r", dev_i2c.addr,config[0]);
+}
+
